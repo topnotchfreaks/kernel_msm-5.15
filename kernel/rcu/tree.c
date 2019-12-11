832c768ad122bc6c20c28e115b49252e36644c89
Yaroslav Furman <yaro330@gmail.com>
2019-12-11 03:06:26 +0300

    rcu: Fix a performance regression.
    Commit "rcu: Create RCU-specific workqueues with rescuers" switched RCU
    to using local workqueses and removed power efficiency flag from them.
    
    This caused a performance regression that can be observed in Geekbench 5
    after enabling CONFIG_WQ_POWER_EFFICIENT_DEFAULT: score went down from
    760/2500 to 620/2300 (single/multi core respectively).
    
    Add WQ_POWER_EFFICIENT flag to avoid this regression.
    
    Change-Id: I2c4f41faa55548f9e81a1c0cbe10703948062d89

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -4869,10 +4869,12 @@ void __init rcu_init(void)
 		rcutree_online_cpu(cpu);
 	}
 
-	/* Create workqueue for Tree SRCU and for expedited GPs. */
-	rcu_gp_wq = alloc_workqueue("rcu_gp", WQ_MEM_RECLAIM | WQ_UNBOUND, 0);
+	/* Create workqueue for expedited GPs and for Tree SRCU. */
+	rcu_gp_wq = alloc_workqueue("rcu_gp", WQ_POWER_EFFICIENT | WQ_MEM_RECLAIM, 0);
 	WARN_ON(!rcu_gp_wq);
-	rcu_alloc_par_gp_wq();
+	rcu_par_gp_wq = alloc_workqueue("rcu_par_gp", WQ_POWER_EFFICIENT | WQ_MEM_RECLAIM, 0);
+	WARN_ON(!rcu_par_gp_wq);
+	srcu_init();
 
 	/* Fill in default value for rcutree.qovld boot parameter. */
 	/* -After- the rcu_node ->lock fields are initialized! */
