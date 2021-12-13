32bc89da52522bd0eb61431f0c8376c5d798397f
Akash Asthana <akashast@codeaurora.org>
2021-12-13 13:02:56 +0100

    serial: msm_geni_serial: Register normal suspend instead of suspend_noirq
    While executing suspend_noirq callbacks non-wakeup interrupts are
    disabled, wake-up interrupts are enabled but their handling is
    deferred till the completion of suspend_noirq stage.
    
    UART console vote/unvote the resources in suspend_noirq/resume_noirq
    stages. There is no issue wrt managing clk and pinctrl resources at
    noirq stage however ICC BW voting can't be done at noirq stage.
    
    ICC requests are converted to ack based requested for RPM driver, the
    ack here is nothing but interrupt from IPCC driver and it can't be
    processed at noirq stage, hence flow is indefinitely stuck in UART
    suspend_noirq callback.
    
    Change-Id: Ie279580795d3c85db4dc42766b0b2d9a8adb3f00
    Signed-off-by: Akash Asthana <akashast@codeaurora.org>
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Signed-off-by: delmontaro <jagaddhitajalu@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    [muralivijay: Adapt 4.14 changes for 4.19]
    Signed-off-by: Muralidharan Kumaresan <muralidharan9845@gmail.com>

--- drivers/tty/serial/msm_geni_serial.c
+++ drivers/tty/serial/msm_geni_serial.c
@@ -5592,7 +5592,7 @@ static int msm_geni_serial_sys_suspend(struct device *dev)
 	return 0;
 }
 
-static int msm_geni_serial_sys_hib_resume(struct device *dev)
+static int msm_geni_serial_sys_resume(struct device *dev)
 {
 	struct platform_device *pdev = to_platform_device(dev);
 	struct msm_geni_serial_port *port = platform_get_drvdata(pdev);
@@ -5694,20 +5694,13 @@ static int msm_geni_serial_sys_resume(struct device *dev)
 {
 	return 0;
 }
-static int msm_geni_serial_sys_hib_resume(struct device *dev)
-{
-	return 0;
-}
 #endif
 
 static const struct dev_pm_ops msm_geni_serial_pm_ops = {
 	.runtime_suspend = msm_geni_serial_runtime_suspend,
 	.runtime_resume = msm_geni_serial_runtime_resume,
-	.suspend = msm_geni_serial_sys_suspend,
-	.resume = msm_geni_serial_sys_resume,
-	.freeze = msm_geni_serial_sys_suspend,
-	.restore = msm_geni_serial_sys_hib_resume,
-	.thaw = msm_geni_serial_sys_hib_resume,
+	.suspend_noirq = msm_geni_serial_sys_suspend,
+	.resume_noirq = msm_geni_serial_sys_resume,
 };
 
 static struct platform_driver msm_geni_serial_platform_driver = {
