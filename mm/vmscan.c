BayLibre/android_kernel_common

ANDROID: mm: export isolate_folio and reclaim_pages
export isolate_folio and reclaim_pages to support to reclaim pages in ko

Bug: 390332073
Change-Id: Ib224548baed1217ef96cd3974775c8dc65e77a50
Signed-off-by: yipeng xiang <yipengxiang@honor.corp-partner.google.com>

mm/vmscan.c
@@ -2197,6 +2197,7 @@ unsigned long reclaim_pages(struct list_head *folio_list)
 
 	return nr_reclaimed;
 }
+EXPORT_SYMBOL_GPL(reclaim_pages);
 
 static unsigned long shrink_list(enum lru_list lru, unsigned long nr_to_scan,
 				 struct lruvec *lruvec, struct scan_control *sc)
@@ -4368,7 +4369,7 @@ static bool sort_folio(struct lruvec *lruvec, struct folio *folio, struct scan_c
 	return false;
 }
 
-static bool isolate_folio(struct lruvec *lruvec, struct folio *folio, struct scan_control *sc)
+bool isolate_folio(struct lruvec *lruvec, struct folio *folio, struct scan_control *sc)
 {
 	bool success;
 
@@ -4401,6 +4402,7 @@ static bool isolate_folio(struct lruvec *lruvec, struct folio *folio, struct sca
 
 	return true;
 }
+EXPORT_SYMBOL_GPL(isolate_folio);
 
 static int scan_folios(struct lruvec *lruvec, struct scan_control *sc,
 		       int type, int tier, struct list_head *list)

topnotchfreaks/kernel_msm-5.15

Conflicts:
	mm/vmscan.c
	isolate_folio -> isolate_page
(cherry picked from commit 0e47a7391b3e2e57c4769f14a7be0ba7b56b10ca)
(cherry picked from commit b4c5ce4fc75510ee4e1051ee1d608ad2307ef738)
Signed-off-by: Hailong Liu <liuhailong@oppo.com>

mm/vmscan.c
@@ -4610,7 +4610,7 @@ static bool sort_page(struct lruvec *lruvec, struct page *page, struct scan_cont
 	return false;
 }
 
-static bool isolate_page(struct lruvec *lruvec, struct page *page, struct scan_control *sc)
+bool isolate_page(struct lruvec *lruvec, struct page *page, struct scan_control *sc)
 {
 	bool success;
 
@@ -4647,6 +4647,7 @@ static bool isolate_page(struct lruvec *lruvec, struct page *page, struct scan_c
 
 	return true;
 }
+EXPORT_SYMBOL_GPL(isolate_page);
 
 static int scan_pages(struct lruvec *lruvec, struct scan_control *sc,
 		      int type, int tier, struct list_head *list)