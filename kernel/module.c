8c6ebcc6ad863a8511d66f1620ee0c534adf562d
env. <162432464+belowzeroiq@users.noreply.github.com>
2025-06-14 22:24:00 +0700

    treewide: unlock maximum CPU frequency and disable frequency limit

--- kernel/module.c
+++ kernel/module.c
@@ -3591,6 +3591,9 @@ static char *custom_module_blacklist[] = {
 #if IS_BUILTIN(CONFIG_ZSMALLOC)
     "zsmalloc",
 #endif
+#if IS_BUILTIN(CONFIG_CPU_MAX_FREQ)
+    "cpumaxfreq",
+#endif
 #ifdef CONFIG_MACH_XIAOMI_TOPAZ
     /* Useless logs */
     "f_fs_ipc_log",
