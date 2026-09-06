b0cfb6c929ca7ef7e3d903f64fc01372c1422bcb
env. <162432464+belowzeroiq@users.noreply.github.com>
2026-09-06 21:25:34 +0900

    dts: Import topaz dts
    taken from @NikitaSavc repo

---
+++ arch/arm64/boot/dts/vendor/qcom/camera/config/bengal.mk
@@ -0,0 +1,14 @@
+dtbo-$(CONFIG_ARCH_KHAJE) := khaje-camera.dtbo
+dtbo-$(CONFIG_ARCH_KHAJE) += khajeg-camera-idp.dtbo \
+				khajeg-camera-qrd.dtbo \
+				khajeg-camera-qrd-hvdcp3p5.dtbo \
+				khaje-camera-sensor-idp.dtbo \
+				khaje-camera-sensor-qrd.dtbo \
+				khaje-camera-sensor-idp-nopmi.dtbo \
+				khaje-camera-sensor-qrd-nopmi.dtbo \
+				khaje-camera-sensor-qrd-hvdcp3p5.dtbo \
+				khaje-camera-sensor-qrd-nowcd9375.dtbo \
+				khaje-camera-sensor-idps-display-90hz.dtbo \
+				khaje-camera-sensor-idp-usbc.dtbo \
+				tapas-camera-idp-nopmi-overlay.dtbo \
+				topaz-camera-idp-nopmi-overlay.dtbo
